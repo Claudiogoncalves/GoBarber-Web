f3f123a18fb5f2bc71bcc0a10f61254e2edc530c
Claudiogoncalves <claudiogoncalvesrp@gmail.com>
2020-05-23 07:51:10 +0100

    Create page login and logout

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,69 @@
+import React, { useRef, useCallback } from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErros from '../../utils/getValidationErrors';
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Password obrigatório'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log(error);
+
+      const errors = getValidationErros(error);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="Gobarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu login</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Password"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqeci minha senha</a>
+        </Form>
+
+        <a href="forgot">
+          <FiLogIn />
+          Criar Conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default SignIn;
